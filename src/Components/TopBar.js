d0fd77c3d48defc367e7327fb57005a92780fb80
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2021-08-14 22:34:17 +0700

    Mencoba Routing

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,18 +1,80 @@
 import React from "react";
 import logo from "../spotify-logo.svg";
+import HomeTest from "./HomeTest";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 class TopBar extends React.Component {
     render() {
         return(
-          <div className={"top-bar"}>
-              <div className={"bar-content"}>
-                  <div className={"brand-logo"}>
-                      <img src={logo} className="app-log" alt="logo" />
-                  </div>
-              </div>
-          </div>
+            <Router>
+                <div className={"top-bar"}>
+                    <div className={"bar-content"}>
+                        <div className={"brand-logo"}>
+                            <Link to={"/"}>
+                                <img src={logo} className="app-log" alt="logo" />
+                            </Link>
+
+                        </div>
+                        <div className={"desktop-control"}>
+                            <ul>
+
+                                <li>
+                                    <Link to={"/premium"}>Premium</Link>
+                                </li>
+                                {/*<li>*/}
+                                {/*    <Link to={"/dukungan"}>Dukungan</Link>*/}
+                                {/*</li>*/}
+                                {/*<li>*/}
+                                {/*    <Link to={"/download"}>Download</Link>*/}
+                                {/*</li>*/}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <Switch>
+                    <Route exact path={"/premium"}>
+                        <HomeTest />
+                    </Route>
+                    {/*<Route path="/dukungan">*/}
+                    {/*    <About />*/}
+                    {/*</Route>*/}
+                    {/*<Route path="/download">*/}
+                    {/*    <Dashboard />*/}
+                    {/*</Route>*/}
+                </Switch>
+            </Router>
         );
     }
 }
+
+// function Home() {
+//     return (
+//         <div>
+//             <h2>Home</h2>
+//         </div>
+//     );
+// }
+//
+// function About() {
+//     return (
+//         <div>
+//             <h2>About</h2>
+//         </div>
+//     );
+// }
+//
+// function Dashboard() {
+//     return (
+//         <div>
+//             <h2>Dashboard</h2>
+//         </div>
+//     );
+// }
+
 
 export default TopBar;
